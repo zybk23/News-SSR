5506c758444e711aceea7741ac3a4f050037cb84
furkangedik <furkangedik@protonmail.ch>
2023-10-09 22:08:40 +0300

    changes

--- app/(root)/newsDetails/page.tsx
+++ app/(root)/newsDetails/page.tsx
@@ -1,46 +1,7 @@
-"use client";
-
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
-import { useAppSelector } from "../../../store/hooks";
-import { Button, AddRemoveList } from "../../../components";
-import { articlesTypes } from "../../../helpers/types";
-import moment from "moment";
-import "./style.scss";
+import NewsDetailPage from "@/components/News-Detail";
 
 const NewsDetail = () => {
-  const router = useRouter();
-  const [articlesInStorage, setArticlesInStorage] = useState(
-    typeof window !== "undefined" &&
-      JSON.parse(window.localStorage.getItem("readList") || "[]")
-  );
-  const { selectedArticle }: { selectedArticle: articlesTypes } =
-    useAppSelector((state) => state.dataSlice);
-
-  const handleGoBackToListPage = () => {
-    router.back();
-  };
-
-  return (
-    <div className="detail-page-container">
-      <div className="detail-container">
-        <span className="detail-title">{selectedArticle?.title}</span>
-        <img className="detail-img" src={selectedArticle?.urlToImage} alt="" />
-        <span className="detail-content">{selectedArticle?.content}</span>
-        <AddRemoveList
-          hour={moment(selectedArticle?.publishedAt).format("hh:mm")}
-          articleSourceId={
-            selectedArticle?.source?.id + "-" + selectedArticle?.id
-          }
-          articlesInStorage={articlesInStorage}
-          setArticlesInStorage={setArticlesInStorage}
-        />
-        <div style={{ marginTop: "45px" }}>
-          <Button text="Go to Back" handleGoBack={handleGoBackToListPage} />
-        </div>
-      </div>
-    </div>
-  );
+  return <NewsDetailPage />;
 };
 
 export default NewsDetail;
